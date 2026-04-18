9b56fe6598fc533bf4b43e90c8e55e3294613a56
amitsharma0008 <asksharma2603@gmail.com>
2026-04-18 16:25:38 +0530

    update

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://rwkgmnhtgahfvzmevmgv.supabase.co"
+const supabaseKey = "sb_publishable_ICAkWwZuscIEPUkX7bJg9w_zlpqhWbT"
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
