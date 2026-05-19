88c2292810477bdf63db084c6478efb1cdf86282
amitsharma0008 <asksharma2603@gmail.com>
2026-05-19 12:30:41 +0530

    commit

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { supabase } from "../supabaseClient";
+import { useNavigate } from "react-router-dom";
+
 
 function ResetPassword() {
   const [password, setPassword] = useState("");
